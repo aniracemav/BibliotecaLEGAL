8355f67a52d1d525defaacac260c33e59569c287
aniracemav <anaidvazderecho@gmail.com>
2026-07-28 18:16:39 -0600

    Delete pages/red-notarias directory

--- pages/red-notarias/notarias.js
+++
@@ -1,5 +0,0 @@
-
-const RN_ENDPOINT = 'https://api.pulppo.com/legals/events';
-window.addEventListener('DOMContentLoaded', async () => {
-  try{ await fetch(RN_ENDPOINT, { method:'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify({ type:'visit' })}); }catch(e){}
-});
